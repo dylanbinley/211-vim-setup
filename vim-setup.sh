0c2fc58e328ccc34a3fda86ceb4ef1ceafe43b39
Your Name <you@example.com>
2020-11-25 21:58:00 -0500

    check if vundle already installed

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,13 +1,24 @@
 #!/bin/sh -eux
 export DEBIAN_FRONTEND=noninteractive
-sudo apt install build-essential cmake python3-dev
 
+#install necessary tools
+sudo apt install build-essential cmake python3-dev vim 
+
+#copy over configuration files
 cp ./.ycm_extra_conf.py ~/.ycm_extra_conf.py
 cp ./.vimrc-final-with-ycm ~/.vimrc
 
+#install plugins
 mkdir -p ~/.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim -c 'PluginInstall' -c 'qa!'
+if [ -e ~/.vim/bundle/Vundle.vim ]
+then
+    echo "Vundle already installed"
+else
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+#yes included to skip over unwanted messages in terminal 
+yes | vim -c 'PluginInstall' -c 'qa!'
 
+#finish YouCompleteMe plugin installation
 cd ~/.vim/bundle/YouCompleteMe
 python3 install.py --clang-completer
