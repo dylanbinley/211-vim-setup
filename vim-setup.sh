f48cdc78af1a806ccaad449220e9d2a5c538e435
Your Name <you@example.com>
2020-11-25 20:22:46 -0500

    copy vimrc file over

--- vim-setup.sh
+++ vim-setup.sh
@@ -2,6 +2,9 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo apt install build-essential cmake python3-dev
 
+cp ./.ycm_extra_conf.py ~/.ycm_extra_conf.py
+cp ./.vimrc-final-with-ycm ~/.vimrc
+
 mkdir -p ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c 'PluginInstall' -c 'qa!'
